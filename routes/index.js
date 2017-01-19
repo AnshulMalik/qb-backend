1808b1038d9a2361553dc9f2b0b244de1fcb4b6b
Anshul <malikanshul29@gmail.com>
2017-01-19 17:19:41 +0530

    Live contests

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-var contests = require('../lib/api/contests');
 var auth = require('../lib/api/auth');
 
-router.use('/contests', contests);
 router.use('/auth', auth);
 
 module.exports = router;
