c4fc60841aee23cdfa0dd55e87a6fbce97aba8c8
Anshul <malikanshul29@gmail.com>
2017-01-25 17:13:37 +0530

    Changed answer submit response

--- qb-backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
-var auth = require('../lib/api/auth');
-
-router.use('/auth', auth);
-
-module.exports = router;
--- qb-backend/test.js
+++
@@ -1,9 +0,0 @@
-let words = require('./english');
-
-require('./lib/api/db').then((db ) => {
-    db.collection('dictionaries').insertOne({
-        words,
-        language: 'english',
-        topic: 'english'
-    })
-})
